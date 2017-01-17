75a6159a8c1e279e77286e2182b404e077745c87
Alexander <aw19@sanger.ac.uk>
2017-01-17 12:05:25 +0000

    commandline options

--- tronprep.py
+++ tronprep.py
@@ -6,9 +6,6 @@ import csv
 import tempfile
 import subprocess
 
-class SampleData:
-	def __init__(self,sample_name):
-		self.sample_name = sample_name
 
 # input data
 parser = argparse.ArgumentParser(
@@ -19,20 +16,11 @@ parser.add_argument('file_of_trait_fastqs', help='File of filenames of each trai
 parser.add_argument('file_of_nontrait_fastqs', help='File of filenames of each nontrait isolate')
 options = parser.parse_args()
 
-trait_samples = []
-#read in metadata
-with open(options.file_of_trait_fastqs) as traitfofn:
-    for isolate in traitfofn:
-        trait_samples.append(isolate)
-
-#with open(options.file_of_trait_fastqs) as fofnfile:
- #   for samples in fofn:
-  #      trait_samples.append( (SampleData(sample))
 
 
 working_dir = os.getcwd()
 trait_fileactual = working_dir+"/trait_filename.fofn"
-trait_infile = trait_samples
+trait_infile = options.file_of_trait_fastqs
 with open(trait_fileactual, 'w') as trait_outfile:        
     for isolate in trait_infile:
         forward_read = isolate+"_1.fastq.gz"
@@ -41,5 +29,3 @@ with open(trait_fileactual, 'w') as trait_outfile:
         isolate_reverse_read = reverse_read
         trait_writer = csv.writer(trait_outfile, delimiter=',', lineterminator='\n')
         trait_writer.writerow([isolate_foward_read]+[isolate_reverse_read])
-   #row_entry = isolate.foward_read+isolate.reverse_read
-  # print(isolate.foward_read+isolate.reverse_read)
