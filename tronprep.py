d0267510f57636f17f71ee5618ad19ea5ddc1b5d
alexwailan <alex.wailan24@gmail.com>
2017-04-13 14:57:27 +0100

    Final_script
    Fresh but not clean

--- tronprep.py
+++ tronprep.py
@@ -9,23 +9,32 @@ import subprocess
 
 # input data
 parser = argparse.ArgumentParser(
-	description = 'Simply prepping the fastq files',
-	usage = 'TronPrep [options] file_of_trait_fastqs file_of_nontrait_fastqs output_directory')
-parser.add_argument('output_directory', help='Output directory')
+	description = 'Simply preparing the fastq files',
+	usage = 'TronPrep [options] file_of_trait_fastqs file_of_nontrait_fastqs')
 parser.add_argument('file_of_trait_fastqs', help='File of filenames of each trait isolate')
 parser.add_argument('file_of_nontrait_fastqs', help='File of filenames of each nontrait isolate')
 options = parser.parse_args()
 
-
-
 working_dir = os.getcwd()
-trait_fileactual = working_dir+"/trait_filename.fofn"
+trait_output_file  = working_dir+"/trait_file_of_fastqs.csv"
+non_trait_output_file = working_dir+"/nontrait_file_of_fastqs.csv"
+
 trait_infile = options.file_of_trait_fastqs
-with open(trait_fileactual, 'w') as trait_outfile:        
-    for isolate in trait_infile:
-        forward_read = isolate+"_1.fastq.gz"
-        reverse_read = isolate+"_2.fastq.gz"
-        isolate_foward_read = forward_read
-        isolate_reverse_read = reverse_read
-        trait_writer = csv.writer(trait_outfile, delimiter=',', lineterminator='\n')
-        trait_writer.writerow([isolate_foward_read]+[isolate_reverse_read])
+nontrait_infile = options.file_of_nontrait_fastqs
+with open(trait_infile, "r") as inputdata:
+    with open(trait_output_file, 'w') as outputfile:        
+        for isolate in inputdata:
+            isolate_name = isolate.strip()
+            isolate_foward_read = isolate_name + "_1.fastq.gz"
+            isolate_reverse_read = isolate_name + "_2.fastq.gz"
+            trait_writer = csv.writer(outputfile, delimiter=',', lineterminator='\n')
+            trait_writer.writerow([str(isolate_foward_read)]+[str(isolate_reverse_read)])
+
+with open(nontrait_infile, "r") as inputdata:
+    with open(non_trait_output_file, 'w') as outputfile:        
+        for isolate in inputdata:
+            isolate_name = isolate.strip()
+            isolate_foward_read = isolate_name + "_1.fastq.gz"
+            isolate_reverse_read = isolate_name + "_2.fastq.gz"
+            trait_writer = csv.writer(outputfile, delimiter=',', lineterminator='\n')
+            trait_writer.writerow([str(isolate_foward_read)]+[str(isolate_reverse_read)])
